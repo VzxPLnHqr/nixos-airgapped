b0c1029ace9114f1a0d6a41fe985a74104e27663
VzxPLnHqr <VzxPLnHqr@proton.me>
2024-05-13 20:13:36 +0000

    refactor, allow for easier vm testing

--- flake.nix
+++ flake.nix
@@ -12,28 +12,35 @@
 
   outputs = inputs@{ self, nixpkgs, nixos-generators }: {
 
-    packages.x86_64-linux.nixos-airgapped-iso = nixos-generators.nixosGenerate {
-        
-        system = "x86_64-linux";
 
-        modules = [
+    nixosModules.nixos-airgapped = {pkgs, config, ...}: {
 
-          ./configuration.nix
+            imports = [ ./configuration.nix ];
 
-          ({ pkgs, config, ... }: {
             # Let 'nixos-version --json' know about the Git revision
             # of this flake.
             system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-          })
-          
-        ];
-
-        # see different formats here: https://github.com/nix-community/nixos-generators
-        # for nixos bootable installer ISO change to "install-iso"
-        format = "iso";
     };
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    # build a vm by running `nixos-rebuild build-vm .#nixos-airgapped`
+    # the vm can be started by running `./result/bin/run-...`
+    nixosConfigurations.nixos-airgapped = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ self.nixosModules.nixos-airgapped ];
+    };
+
+    # build the iso by running `nix build .#nixos-airgapped-iso`
+    packages.x86_64-linux.nixos-airgapped-iso = nixos-generators.nixosGenerate {
+      system = "x86_64-linux";
+      modules = [ self.nixosModules.nixos-airgapped ];
+
+      # see different formats here: https://github.com/nix-community/nixos-generators
+      # for nixos bootable installer ISO change to "install-iso"
+      format = "iso";
+    };
+    
+    # by default we build the .iso (`nix build .#nixos-airgapped-iso)
+    packages.x86_64-linux.default = self.packages.x86_64-linux.nixos-airgapped-iso;
 
   };
 }
