c30b23bfc2e2eb54caabd4abc43de7bd4f7878e9
VzxPLnHqr <VzxPLnHqr@proton.me>
2024-05-15 19:30:01 +0000

    finally fixed codex32 website (was trying to host files from wrong directory...argggh!)

--- modules/codex32.nix
+++ modules/codex32.nix
@@ -1,21 +1,9 @@
 { lib, pkgs, config, codex32-website, ... }: {
 
-  # host the codex32 website offline
-  #  THIS IS BROKEN
-  #   for some strange reason, nginx does not serve the site up properly
-  #   however, manually running the following *does* seem to work:
-  #
-  #   cd /nix/store/18aakaqqgi2a4dam0zq80v6gkmb1r41k-codex32-website-1.0.0/www
-  #   python3 webserver.py
-  #   # now the site is accessible at http://localhost:9000
-  #
-  #  but why does the below simple nginx site serving out of the same directory
-  #  not work? WHY!!!? 
-
-  services.nginx = {
+	services.nginx = {
     enable = true;
     virtualHosts."codex32.offline" = {
-      root = "${codex32-website}/www";
+        root = "${codex32-website.packages.x86_64-linux.codex32-website}/www";
     };
   };
 
